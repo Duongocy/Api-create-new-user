ed80fcd92b924f1ce841c5d411e5ad2ccaa83a19
Y Duong Ngoc <ydn@hoya.com>
2025-07-10 11:20:12 +0700

    kiem tra user ton tai hay chua 5

--- create_new_user.js
+++ create_new_user.js
@@ -20,10 +20,7 @@ app.get('/Invoice',async (yeucaune,traloine) =>{
     const ten_user = yeucaune.query.username;
     console.log("Tên cần kiểm tra nè : ",ten_user);
     try {
-            const ket_qua_kiem_tra_ton_tai = await pool.query(
-            'SELECT 1 FROM user_table WHERE user_name = $1 LIMIT 1',
-            [ten_user]
-            );
+            const ket_qua_kiem_tra_ton_tai = await pool.query('SELECT 1 FROM user_table WHERE user_name = $1 LIMIT 1',ten_user);
             traloine.json({ exists: result.rows.length > 0 });
         } 
     catch (err) {
