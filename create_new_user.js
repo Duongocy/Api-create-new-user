465cd79bac3ef907c647c9282cf24b445fbbe4fb
Y Duong Ngoc <ydn@hoya.com>
2025-07-10 09:47:16 +0700

    kiem tra user ton tai hay chua 2

--- create_new_user.js
+++ create_new_user.js
@@ -17,8 +17,8 @@ const pool = new Pool({
 
 //Kiểm tra xem user name đã tồn tại chưa 
 app.get('/Invoice',async (yeucaune,traloine) =>{
-    const {ten_user} = yeucaune.query;
-    console.log("Tên cần kiểm tra nè : ",{ten_user});
+    const ten_user = yeucaune.query.username;
+    console.log("Tên cần kiểm tra nè : ",ten_user);
 })
 // Thêm dữ liệu user vào bảng user
 app.post('/Invoice', async (req, res) => {
