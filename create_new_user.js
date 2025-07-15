f247d7479dfb7f463138331d2e264b348b38b3e7
Dương Ngọc Ý <duongocy@gmail.com>
2025-07-15 22:08:11 +0700

    xu ly thong tin dang nhap 2

--- create_new_user.js
+++ create_new_user.js
@@ -43,13 +43,13 @@ app.get('/Invoice',async (yeucaune,traloine) =>{
                 // Kiểm tra xem có bản ghi nào không
                 if (ket_qua_kiem_tra_ton_tai.rows.length > 0) {
                 // Nếu có, trả kết quả về client
-                    res.status(200).json({
+                    traloine.status(200).json({
                     status: 'success',
                     data: ket_qua_kiem_tra_ton_tai.rows[0] // Gửi thông tin người dùng đầu tiên tìm thấy
                     });
                 } else {
                 // Nếu không có, thông báo không tìm thấy
-                    res.status(404).json({
+                    traloine.status(404).json({
                     status: 'fail',
                     message: 'Không tìm thấy người dùng'
                     });
